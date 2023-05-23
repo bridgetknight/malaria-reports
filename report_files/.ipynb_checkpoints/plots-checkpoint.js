4a574f70841e494a087b528e364b13c9078ed9ea
bridgetknight <bknight@broadinstitute.org>
2023-05-23 01:35:08 +0000

    Initial commit

---
+++ report_files/.ipynb_checkpoints/plots-checkpoint.js
@@ -0,0 +1,55 @@
+QSCORES = document.getElementById('qscores');
+
+var layout = {
+    width: 500,
+    height: 00,
+    margin: {
+        l: 95,
+        r: 80,
+        b: 90,
+        t: 10
+    },
+    paper_bgcolor:'rgba(0,0,0,0)',
+    plot_bgcolor:'rgba(0,0,0,0)',
+    modebar: {
+        // vertical modebar button layout
+        orientation: 'v',
+        // for demonstration purposes
+        bgcolor: 'white',
+        color: '#a7a8a8',
+        activecolor: '#9ED3CD'
+      },
+    
+      // move the legend inside the plot on the top right
+      xaxis: {
+        title: {
+          text: 'Q Score',
+          font: {
+            family: 'Rubik',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        },
+      },
+      yaxis: {
+        title: {
+          text: 'Number of Reads',
+          font: {
+            family: 'Rubik',
+            size: 18,
+            color: '#7f7f7f'
+          }
+        }
+      }
+    
+}
+
+var config = {
+    displayModeBar: true // or 'true' for always visible
+  };
+    
+Plotly.plot(QSCORES, [{
+  type: 'scatter',
+  x: [5, 7, 10, 12, 15],
+  y: [3413205, 3413204, 3413073, 3062945, 2120402]
+}], layout, config)
